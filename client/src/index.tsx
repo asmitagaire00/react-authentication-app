aef8e5b0426eb1c08e467e7ecff73dc0a3813100
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-12 21:25:34 +1000

    setup aut0 react sdk

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>
 );
